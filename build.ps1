f230030485cd749d2d3c1861c8957586748876b6
Bradley Grainger <bgrainger@gmail.com>
2013-03-19 11:17:16 -0700

    Add build script to create the NuGet package.

---
+++ build.ps1
@@ -0,0 +1,14 @@
+Task Default -depends SourceIndex
+
+Task Build {
+  Exec { msbuild /m:4 /property:Configuration=Release Logos.Git.sln }
+}
+
+Task SourceIndex -depends Build {
+  $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
+  Exec { tools\SourceIndex\github-sourceindexer.ps1 -symbolsFolder src\Logos.Git\bin\Release -userId LogosBible -repository Logos.Git -branch $headSha -sourcesRoot ${pwd} -dbgToolsPath "C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86" -gitHubUrl "https://raw.github.com" -serverIsRaw -verbose }
+}
+
+Task NuGetPack -depends SourceIndex {
+  Exec { nuget pack src\Logos.Git\Logos.Git.csproj -Prop Configuration=Release -Symbols }
+}
