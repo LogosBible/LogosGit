73400494996f4811892d73f29fd292621541bbaf
Bradley Grainger <bgrainger@gmail.com>
2014-03-26 14:12:21 -0700

    Ignore paths to get source indexing working again.

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ Task Build {
 
 Task SourceIndex -depends Build {
   $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
-  Exec { tools\SourceIndex\github-sourceindexer.ps1 -symbolsFolder src\Logos.Git\bin\Release -userId LogosBible -repository Logos.Git -branch $headSha -sourcesRoot ${pwd} -dbgToolsPath "C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86" -gitHubUrl "https://raw.github.com" -serverIsRaw -verbose }
+  Exec { tools\SourceIndex\github-sourceindexer.ps1 -symbolsFolder src\Logos.Git\bin\Release -userId LogosBible -repository Logos.Git -branch $headSha -sourcesRoot ${pwd} -dbgToolsPath "C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86" -gitHubUrl "https://raw.github.com" -serverIsRaw -verbose -ignore libgit2sharp, "c:\program files", "C:\Users", "f:\binaries", "f:\dd" }
 }
 
 Task NuGetPack -depends SourceIndex {
